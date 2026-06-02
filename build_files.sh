3d60486a0781457e5f88f8edb2115e047279f58c
Wafula <tofina41@gmail.com>
2026-06-02 23:47:35 +0300

    update

--- build_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "==> Installing workspace dependencies..."
-pip install -r requirements.txt
-
-echo "==> Compiling server-level Django static assets..."
-python manage.py collectstatic --noinput --clear
